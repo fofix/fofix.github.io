487cebce0518b189fcd5f1b90e8b5f1c16433f56
François Magimel <magimel.francois@gmail.com>
2018-01-10 21:12:37 +0100

    Init the Pelican conf

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'FoFiX team'
+SITENAME = 'Frets On Fire X — FoFiX'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('Frets On Fire', 'http://fretsonfire.sourceforge.net'),
+)
+
+# Social widget
+SOCIAL = (
+    ('GitHub', 'https://github.com/fofix'),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
